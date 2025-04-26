34e9cbd8cbd21d133c153fc6785fb4354ef225ec
Satyam <www.satyam200413@gmail.com>
2025-04-26 11:59:53 +0530

    Cmmpleted hero section of this music course website where i uses aceternity ui components for designing the page section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import { main } from "motion/react-client";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02">
-          <h1 className="text-2xl text-center text-red-400">Hello from Satyam</h1>
+          <HeroSection/>
     </main>
   );
 }
